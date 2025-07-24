c241082e81c6f50a2d2ae6548a7b9e99103655eb
srinidhivijayakumar04 <srinidhivijay04@gmail.com>
2025-07-24 10:37:39 +0530

    Update time.java

--- time.java
+++ time.java
@@ -3,13 +3,13 @@ import java.time.format.DateTimeFormatter;
 
 class Currentdatetime {
     public static void main(String[] args) {
-        // Get current date and time
+        
         LocalDateTime now = LocalDateTime.now();
 
-        // Define format for date and time
+        
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-        // Format and print
+       
         String formattedDateTime = now.format(formatter);
         System.out.println("Current Date and Time: " + formattedDateTime);
     }
